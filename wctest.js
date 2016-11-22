7b60359ec2a6f2afe0b8595e578c28aab9bc120a
seg1 <c2071214@yahoo.com.tw>
2016-11-22 17:16:03 +0800

    wcttest

---
+++ wctest.js
@@ -0,0 +1,12 @@
+function aa002868(){
+
+d3.csv("https://seg1.github.io/SEG1/wordnews.csv", function(data) {
+	console.log(data);
+	      d3.wordcloud()
+        .size([800, 400])
+        .selector('#wordcloud')
+        .words(data)
+        .start();
+});
+
+}
